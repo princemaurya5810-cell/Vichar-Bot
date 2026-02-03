c563159d6cfeff4ac7c157304d100b969fa1d8b2
princemaurya5810-cell <princemaurya5810@gmail.com>
2026-02-03 15:09:00 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# Vichar-Bot ka pehla code
+print("Vichar-Bot ab online hai!")
+
+def start():
+    vichar = "Sahi raste par chalna mushkil ho sakta hai, par manjil khoobsurat hoti hai."
+    print(f"Aaj ka Vichar: {vichar}")
+
+if __name__ == "__main__":
+    start()
+  
